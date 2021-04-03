ddb18b545cb568b63155e3c9e9c9c7ba14f0fc17
Jacob Anderson <jacobdanderson@gmail.com>
2021-04-03 14:14:30 -0600

    General formatting and updates

---
+++ front-end/src/views/Projects.vue
@@ -0,0 +1,123 @@
+<template>
+  <!-------------
+		-   Section   -
+		-------------->
+
+  <section class="projects section_2 index_section">
+    <!--   Aside   -->
+
+    <aside>
+      <h2>Personal Summary</h2>
+      <p>
+        World-class talent driving AI Innovation for Poultry Farming
+        Applications <br /><br />
+
+        AudioT brings together a team of researchers in Acoustics, Machine
+        Learning, and Signal Processing with years of experience in Poultry Farm
+        monitoring. <br /><br />
+
+        We have developed methods of automatically learning audio environments
+        and detecting anomalies including intrusion, sickness, equipment
+        failures, and changes in chicken behavior.  AudioT monitoring technology
+        performs continuous listening to your flocks, providing peace of mind
+        and a record of the flock performance.
+      </p>
+    </aside>
+
+    <!--   Articles   -->
+
+    <div class="flexbox_container">
+      <article class="article_1">
+        <h2>News</h2>
+        <br />
+        <ul>
+          <li>
+            <a href="#"
+              >August 2019 - Under NDA, AudioT has formal cooperation agreements
+              with two top poultry integrators.</a
+            >
+          </li>
+          <li>
+            <a href="#"
+              >July 1, 2019 - AudioT in partnership with Georgia Tech receives
+              National Science Foundation Partnerships for Innovation -
+              Technology Transfer funding.  This funding is directed toward
+              facilitating the translation of cutting-edge research at Georgia
+              Tech to a deployable product.</a
+            >
+          </li>
+          <li>
+            <a href="#"
+              >June 12, 2019 - AudioT presents at ______ .  Here is a link to a
+              video of the presentation.</a
+            >
+          </li>
+          <li>
+            <a href="#"
+              >March 17, 2019 - AudioT presents at  ______ .  Here is a link to
+              a video of the presentation.</a
+            >
+          </li>
+          <li>
+            <a href="#"
+              >December 13, 2019 - AudioT presents at ______ .  Here is a link
+              to a video of the presentation.</a
+            >
+          </li>
+          <li>
+            <a href="#"
+              >November 27, 2018 - the AudioT team completes the NSF Innovation
+              Corps (I-Corps) program.</a
+            >
+          </li>
+        </ul>
+      </article>
+
+      <article class="article_2">
+        <h2><a href="courses.html">Courses Taught</a></h2>
+        <br />
+        <ul>
+          <li>
+            <h4>
+              Current Courses
+              <!-- Currently Taught -->
+            </h4>
+            <ul>
+              <li><a href="ece6254.html">ECE 6254</a></li>
+            </ul>
+          </li>
+          <li>
+            <h4>
+              Previous Courses
+              <!-- Previously Taught -->
+            </h4>
+            <ul>
+              <li><a href="courses.html">ECE 4270</a></li>
+              <li><a href="courses.html">ECE 4271</a></li>
+              <li><a href="courses.html">ECE 2026</a></li>
+              <li><a href="courses.html">ECE 3055</a></li>
+              <li><a href="courses.html">ECE 6250</a></li>
+              <li><a href="courses.html">ECE 6253</a></li>
+              <li><a href="ece6271.html">ECE 6271</a></li>
+              <li><a href="courses.html">ECE 7252</a></li>
+              <li><a href="courses.html">ECE 2030</a></li>
+            </ul>
+          </li>
+        </ul>
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Projects",
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
